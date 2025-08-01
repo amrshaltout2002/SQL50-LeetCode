02644a73a12ce1fe658b09d60ed4914cdbb95b1b
Amr Shaltout <amrshaltout2002@gmail.com>
2025-08-02 00:58:24 +0300

    Update 577. Employee Bonus.sql

--- 577. Employee Bonus.sql
+++ 577. Employee Bonus.sql
@@ -2,4 +2,4 @@ select name, bonus
 from employee e
 left join bonus b
 on e.empId = b.empId
-where e.empId not in (select empId from bonus where bonus > 1000)
+where e.empId not in (select empId from bonus where bonus >= 1000)
