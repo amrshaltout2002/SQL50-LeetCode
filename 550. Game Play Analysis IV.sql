4ecc4958aa5ba50f94d3e504882bb5715f9f8e28
Amr Shaltout <amrshaltout2002@gmail.com>
2025-08-05 23:06:43 +0300

    Add files via upload

---
+++ 550. Game Play Analysis IV.sql
@@ -0,0 +1,8 @@
+
+select round(1.0 * count(player_id) / (select count(distinct player_id) from activity), 2) fraction
+from activity
+where (player_id, event_date) in (
+    select player_id, min(event_date) + 1
+    from activity
+    group by player_id
+)
