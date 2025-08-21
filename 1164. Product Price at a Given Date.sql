1d686a525905bc3020f1252d796888b418c7ae0c
Amr Shaltout <amrshaltout2002@gmail.com>
2025-08-21 12:35:12 +0300

    Add files via upload

---
+++ 1164. Product Price at a Given Date.sql
@@ -0,0 +1,15 @@
+with cte as (
+    select product_id, new_price
+    from products
+    where (product_id, change_date) in (
+        select product_id, max(change_date) last_date
+        from products
+        where change_date <= '2019-08-16'
+        group by product_id
+    )
+)
+
+select distinct p.product_id, coalesce(cte.new_price, 10) as price
+from products p
+left join cte
+on p.product_id = cte.product_id
